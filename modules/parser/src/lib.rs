fa693654f6cf809c129826ba807bfda1bf599ce2
Salenia <vsreui123456@gmail.com>
2020-12-26 21:03:55 +0200

    Add base struct of parser

--- modules/parser/src/lib.rs
+++ modules/parser/src/lib.rs
@@ -2,6 +2,17 @@ extern crate common;
 
 mod lexer;
 
+use lexer::Token;
+
+pub struct Parser<'a> {
+    content: &'a [Token],
+    index: usize,
+}
+
+// impl<'a> common::Builder for Parser<'a> {
+//     type Input = &'a [Token];
+//     type Output = Result<..., String>;
+// }
 #[cfg(test)]
 mod tests {
     use super::*;
