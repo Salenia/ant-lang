d578811e7bfbdc47134df8bdb892b7ebc6513cd8
Salenia <vsreui123456@gmail.com>
2020-12-26 17:47:06 +0200

    remove implementations

--- modules/parser/src/lib.rs
+++ modules/parser/src/lib.rs
@@ -1,11 +1,2 @@
-mod implementations;
-
 #[cfg(test)]
-mod tests {
-    use super::*;
-    use implementations::location::Location;
-    #[test]
-    fn location_init() {
-        let _loc2 = Location::new(1, 2);
-    }
-}
+mod tests {}
